6d5f629750085d16405095537b77dbe92e2823a1
Rafael Fagundes <rafaelcflima@gmail.com>
2024-09-05 00:55:17 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Rafael Fagundes on 2024-09-05.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
