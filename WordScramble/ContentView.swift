3022946a274b9553db5c5459f5f86003dfa524a6
Rafael Fagundes <rafaelcflima@gmail.com>
2024-09-06 00:21:34 -0400

    feat: improve ui, ux and gameplay

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,84 +12,150 @@ struct ContentView: View {
   @State private var usedWords: [String] = []
   @State private var rootWord: String = ""
   @State private var newWord: String = ""
-
+  
   @State private var errorTitle = ""
   @State private var errorMessage = ""
   @State private var showingError = false
-
+  
+  @State private var score = 0
+  let scoreSize: Double = 40
+  
+  @State private var isKeyboardVisible = false
+  
   @FocusState private var isTextFieldFocused
+  
+  let purple: Color = Color(red: 0.16, green: 0.07, blue: 0.18, opacity: 1.00)
+  let lightGreen: Color = Color(red: 0.61, green: 0.94, blue: 0.28, opacity: 1.00)
+  let lightBlue: Color = Color(red: 0.67, green: 0.85, blue: 0.89, opacity: 1.00)
+  
+  let boxRadius: CGFloat = 10
+  let horizontalPadding: Double = 20
   
   var body: some View {
     NavigationStack{
-      List{
-        Section {
-          TextField("Enter your word", text: $newWord)
-            .textInputAutocapitalization(.never)
-            .autocorrectionDisabled()
-            .focused($isTextFieldFocused)
-        }
-        
-        Section {
-          ForEach(usedWords, id: \.self) { word in
-            HStack{
-              Image(systemName: "\(word.count).circle")
-              Text(word)
+      VStack{
+        HStack{
+          VStack{
+            ZStack{
+              RoundedRectangle(cornerRadius: 100)
+                .fill(
+                  LinearGradient(gradient: Gradient(colors: [Color.purple, Color(red: 0.82, green: 0.31, blue: 0.31, opacity: 1.00)]),
+                                 startPoint: .top,
+                                 endPoint: .bottom)
+                )
+                .frame(width: scoreSize, height: scoreSize)
+              
+              Text("\(score)")
+                .foregroundStyle(Color.white)
             }
-          }
+            Text("Points").font(.caption).padding(.vertical, 1)
+          }.frame(width: scoreSize * 1.5, height: scoreSize)
+          Spacer()
+          Image("logo")
+            .resizable()
+            .frame(width: 100, height: 100)
+          Spacer()
+          VStack{
+            Button(action: startGame){
+              Image(systemName: "forward.circle.fill").resizable().frame(width: scoreSize, height: scoreSize).foregroundColor(lightGreen)
+            }.frame(width: scoreSize, height: scoreSize)
+            Text("Next Word").font(.caption).padding(.vertical, 1)
+          }.frame(width: scoreSize * 1.5, height: scoreSize)
+        }
+        .padding(.horizontal, horizontalPadding)
+        StylizedWord(word: rootWord)
+          .padding()
+        
+        TextField("Type your word here", text: $newWord)
+          .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+          .padding()
+          .textInputAutocapitalization(.never)
+          .autocorrectionDisabled()
+          .focused($isTextFieldFocused)
+          .background(Color.black)
+          .cornerRadius(boxRadius)
+          .foregroundStyle(lightBlue)
+          .padding(.horizontal, horizontalPadding)
+          .onSubmit(addNewWord)
+        
+        Text("Create new words using the letters from the word above")
+          .foregroundStyle(lightGreen)
+          .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+          .padding(.horizontal, horizontalPadding)
+          .font(.caption)
+        
+        List{
+            ForEach(usedWords, id: \.self) { word in
+              HStack{
+                Image(systemName: "\(word.count).circle").foregroundStyle(Color.black)
+                Text(word).foregroundStyle(Color.black)
+              }
+            }.listRowBackground(lightBlue)
+        }
+        .scrollContentBackground(.hidden)
+        .onAppear(perform: {
+          startGame()
+        })
+        .alert(errorTitle, isPresented: $showingError) {
+          Button("OK") {}
+        } message: {
+          Text(errorMessage)
         }
       }
-      .navigationTitle(rootWord)
-      .onSubmit(addNewWord)
-      .onAppear(perform: {
-        startGame()
+      .background(purple)
+      .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification), perform: { _ in
+        isKeyboardVisible = true
       })
-      .alert(errorTitle, isPresented: $showingError) {
-        Button("OK") {}
-      } message: {
-        Text(errorMessage)
+      .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification), perform: { _ in
+        isKeyboardVisible = false
+      })
+      .onTapGesture {
+        isTextFieldFocused = false
       }
     }
   }
   
   func addNewWord() {
+    isTextFieldFocused = true
     let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
     
     guard answer.count > 0 else { return }
     
     guard isOriginal(word: answer) else {
-      wordError(title: "Word used already", message: "Be more original!")
+      wordError(title: "Word Already Used", message: "Please enter a word you haven't used before.")
       return
     }
-   
+    
     guard isPossible(word: answer) else {
-      wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+      wordError(title: "Invalid Word", message: "'\(answer)' can't be formed using the letters from '\(rootWord)'.")
       return
     }
     
     guard isReal(word: answer) else {
-      wordError(title: "Word not recognized", message: "Make up words are not accepted!")
+      wordError(title: "Word Not Recognized", message: "'\(answer)' doesn't appear to be a valid English word.")
       return
     }
     
     withAnimation{
       usedWords.insert(answer, at: 0)
     }
-    
-    isTextFieldFocused = true
+    score += answer.count
     newWord = ""
   }
   
   func startGame() {
     isTextFieldFocused = true
+    score = 0
+    usedWords.removeAll()
     if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
       if let startWords = try? String(contentsOf: startWordsURL){
         let allWords = startWords.components(separatedBy: "\n")
-        rootWord = allWords.randomElement() ?? "humorous"
+        rootWord = allWords.randomElement() ?? "scramble"
         return
       }
     }
     
-    fatalError("Could not load start.txt from bundle.")
+    fatalError("Unable to load 'start.txt' from the app bundle.")
   }
   
   func isOriginal(word: String) -> Bool {
@@ -122,6 +188,29 @@ struct ContentView: View {
     errorMessage = message
     showingError = true
     isTextFieldFocused = true
+  }
+}
+
+struct StylizedWord: View {
+  var word: String?
+  var body: some View{
+    getLetters(word: word ?? "error")
+  }
+  
+  func getLetters(word: String) -> some View{
+    let letters = word.map { letter in
+      Image(systemName: "\(letter).square")
+        .resizable()
+        .frame(width: 30, height: 30)
+        .foregroundStyle(Color.white)
+    }
+    return HStack{
+      Spacer()
+      ForEach(letters.indices, id: \.self) {index in
+        letters[index]
+      }
+      Spacer()
+    }
   }
 }
 
